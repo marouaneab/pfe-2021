19271d8b3f87383d0cc1d3762e06611502219d1b
ABABOU MAROUANE <marouaneababou52@gmail.com>
2020-02-15 15:33:55 +0100

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
+import {NgModule, NO_ERRORS_SCHEMA} from '@angular/core';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
@@ -16,9 +16,6 @@ import {
 } from '@angular/material';
 import {DashboardComponent} from './main-page/dashboard.component';
 import {LayoutModule} from '@angular/cdk/layout';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatIconModule} from '@angular/material/icon';
-import {MatListModule} from '@angular/material/list';
 import {FormsModule} from '@angular/forms';
 import {AngularFireModule} from '@angular/fire';
 import {environment} from '../environments/environment';
@@ -26,9 +23,30 @@ import {CollapseComponent} from './tools/collapse/collapse.component';
 import {CollapseTitleComponent} from './tools/collapse/collapse-title/collapse-title.component';
 import {CollapseContentComponent} from './tools/collapse/collapse-content/collapse-content.component';
 import {AdminComponent} from './main-page/dashboard/admin/admin.component';
-import {UserCardComponent} from './user-card/user-card.component';
-import { MailBoxComponent } from './mail-box/mail-box.component';
-import { MessageComponent } from './mail-box/message/message.component';
+import { UserCardComponent} from './user-card/user-card.component';
+import {MatCheckboxModule} from '@angular/material';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatSelectModule} from '@angular/material/select';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatCardModule} from '@angular/material/card';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatIconModule} from '@angular/material/icon';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatTableModule} from '@angular/material/table';
+import {MatSortModule} from '@angular/material/sort';
+import {MatPaginatorModule} from '@angular/material/paginator';
 
 @NgModule({
   declarations: [
@@ -39,10 +57,39 @@ import { MessageComponent } from './mail-box/message/message.component';
     CollapseContentComponent,
     AdminComponent,
     UserCardComponent,
-    MailBoxComponent,
-    MessageComponent,
   ],
   imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCheckboxModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    MatInputModule,
+    MatAutocompleteModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatMenuModule,
+    MatSidenavModule,
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatStepperModule,
+    MatTabsModule,
+    MatExpansionModule,
+    MatButtonToggleModule,
+    MatChipsModule,
+    MatIconModule,
+    MatProgressSpinnerModule,
+    MatProgressBarModule,
+    MatDialogModule,
+    MatSnackBarModule,
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
@@ -59,10 +106,11 @@ import { MessageComponent } from './mail-box/message/message.component';
     AngularFireModule.initializeApp(environment.firebaseConfig),
     MatExpansionModule,
     MatDatepickerModule,
-    MatNativeDateModule,
+    MatNativeDateModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas : [NO_ERRORS_SCHEMA]
 })
 export class AppModule {
 }
--- src/app/user-card/user-card.component.ts
+++ src/app/user-card/user-card.component.ts
@@ -1,15 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatTableDataSource} from '@angular/material/table';
 
+export interface Users {
+
+  nom:          string;
+  Uid:          number;
+  photo ?:      string;
+  prenom:       string;
+  email :       string;
+  address:      string;
+  role :        string;
+}
+const USER_DATA: Users[] = [
+  {Uid: 1,  photo : '',nom: 'Ababou',   prenom: 'Marouane',  email : 'marouaneababou52@gmail.com', address: 'Marjane 2 Meknes',role : 'Etudiant'},
+  {Uid: 2,  photo : '',nom: 'Mounir',     prenom: 'Bouaiche',  email : 'Mounir.9@gmail.com', address: 'SebaaYoune',role: 'Etudiant'},
+  {Uid: 3,  photo : '',nom: 'BENNASER',    prenom: 'Mohammed',  email : 'mohamed.bennaser@gmail.com', address: 'Rabat',role : 'Directeur'},
+  {Uid: 4, photo : '',nom: 'BOURAS', prenom: 'Najat',  email : 'najat.bouras@gmail.com', address: 'Meknes',role : 'Secrétariat du Directeur'},
+  {Uid: 5, photo : '',nom: 'BOUACHRINE',     prenom: 'Mohammed',  email : 'mohammed.bouachrine@gmail.com', address: 'Meknes',role : 'Directeur Adjoint'},
+  {Uid: 6, photo : '',nom: 'BENBRAHIM',   prenom: 'Souad',  email : 'benbrahim@est-umi.ac.ma', address: 'Rabat',role : 'Secrétariat du Directeur Adjoint'},
+  {Uid: 7, photo : '',nom: 'BAGHDI',      prenom: 'Mohammed',  email : 'baghdi.mohammed@est-umi.ac.ma', address: 'Rabat',role : 'Secrétaire Général'},
+  {Uid: 8, photo : '',nom: 'CHAHIR',  prenom: 'Saida',  email : 'saida.chahir@est-umi.ac.ma', address: 'Meknes',role : 'Service Scolarité'},
+  {Uid: 9, photo : '',nom: 'REGRAGUI',    prenom: 'Fatiha',  email : 'fatiha.regragui@est-umi.ac.ma', address: 'Rabat',role : 'Chef du Département. Techniques de Management'},
+  {Uid: 10,  photo : '',nom: 'mrani',  prenom: 'nabil',  email : 'nabil.mrani@gmail.com', address: 'meknes',role :'Professeur'},
+  {Uid: 11,  photo : '',nom: 'Lahmer',      prenom: 'Mohammed',  email : 'mohammed.lahmer@gmail.com', address: 'Meknes',role : 'Professeur'},
+  {Uid: 12,  photo : '',nom: 'Addadi',     prenom: 'Amina',  email : 'amina.addadi@gmail.com', address: 'meknes',role : 'Professeur'},
+  {Uid: 13,  photo : '',nom: 'Ghazali',   prenom: 'Yassine',  email : 'yassine.ghazali@gmail.com', address: 'meknes',role : 'professeur'},
+  {Uid: 14,  photo : '',nom: 'EL OUAZZANI',     prenom: 'Rajae',  email : 'elouzzani.rajae@gmail.com', address: 'Casablanca',role :'Professeur'},
+  {Uid: 15,  photo : '',nom: 'CHANA',   prenom: 'Idriss',  email : 'chana.idriss@gmail.com', address: 'Rabat',role : 'Professeur'},
+  {Uid: 16, photo : '',nom: 'BARRADA',       prenom: 'Mohammed',  email : 'Mohammed.barrada@gmail.com', address: 'Rabat',role : 'Professeur'},
+  {Uid: 17, photo : '',nom: 'BENAMAR',     prenom: 'Mariya',  email : 'benamer.mariya@gmail.com', address: 'Meknes',role : 'Professeur'},
+  {Uid: 18, photo : '',nom: 'BENAMER',  prenom: 'Nabil',  email : 'benamer.nabil@gmail.com', address: 'Meknes',role : 'Professeur'},
+  {Uid: 19, photo : '',nom: 'NASIRI',   prenom: 'Samia',  email : 'nasiri.samia@gmail.com', address: 'Rabat',role : 'Professeur'},
+  {Uid: 20, photo : '',nom: 'LOUMMOU',    prenom: 'Brahim',  email : 'brahim.loummou@gmail.com', address: 'Casa',role : 'Professeur'}
+];
 @Component({
   selector: 'app-user-card',
   templateUrl: './user-card.component.html',
-  styleUrls: ['./user-card.component.scss']
+  styleUrls: ['./user-card.component.scss'],
 })
+
+/**
+* @title Table with pagination
+*/
+
 export class UserCardComponent implements OnInit {
 
-  constructor() { }
+displayedColumns: string[] = ['Uid', 'photo','nom', 'prenom', 'email','address','role'];
+dataSource = new MatTableDataSource<Users>(USER_DATA);
+@ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
 
-  ngOnInit(): void {
-  }
+ngOnInit() {
+  this.dataSource.paginator = this.paginator;
+}
 
 }
