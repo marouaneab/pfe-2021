20aa5be777d68f4042ddf7eeb42910728320fe58
ABABOU MAROUANE <marouaneababou52@gmail.com>
2020-02-23 17:52:00 +0100

    events

--- src/app/event-card/events/events.component.ts
+++ src/app/event-card/events/events.component.ts
@@ -1,15 +1,62 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl} from '@angular/forms';
+import {MAT_MOMENT_DATE_FORMATS,MomentDateAdapter,MAT_MOMENT_DATE_ADAPTER_OPTIONS,} 
+            from '@angular/material-moment-adapter';
+import {DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE} from '@angular/material/core';
 
+
+interface Filter {
+  value : string;
+  vValue : string;
+}
+
+interface Fgroup {
+  name : string;
+  type : Filter [];
+}
 @Component({
   selector: 'app-events',
   templateUrl: './events.component.html',
-  styleUrls: ['./events.component.scss']
+  styleUrls: ['./events.component.scss'],
+  providers: [
+    {provide: MAT_DATE_LOCALE, useValue: 'fr-FR'},
+    {
+      provide: DateAdapter,
+      useClass: MomentDateAdapter,
+      deps: [MAT_DATE_LOCALE, MAT_MOMENT_DATE_ADAPTER_OPTIONS]
+    },
+    {provide: MAT_DATE_FORMATS, useValue: MAT_MOMENT_DATE_FORMATS},
+  ],
 })
+
 export class EventsComponent implements OnInit {
 
-  constructor() { }
+  FilterControl =new FormControl('');
+  FGroups : Fgroup [] = [ {
+    name : 'Date',
+    type : [
+      {value : 'today' ,vValue : `Aujourd'hui`},
+      {value : 'week',vValue : 'Cette semaine'},
+      {value : 'month' , vValue : 'Cet moins'}
+    ]
+  },
+ {   name : 'Status',
+ type : [
+   {value : 'upcoming' ,vValue : 'Prochaine'},
+   {value : 'in review',vValue : 'En révision'},
+   {value : 'in review',vValue : 'Publié'},
+   {value : 'cancelled' , vValue : 'Annulés'},
+   {value : 'overdue', vValue : 'En retard'}
+ ]
+
+ }
+]
+
+  constructor(private _adapter: DateAdapter<any>) { }
 
   ngOnInit(): void {
   }
-
+  french() {
+    this._adapter.setLocale('fr');
+  }
 }
